8ee2391e4f03013483d0b7e3dace45dc5c7ef5d3
noeurng <oi.nuan@gmail.com>
2016-08-17 15:11:34 +0700

    Initial commit

---
+++ app/src/main/java/navigation_drawer/noeurng/com/intro_slide/MainActivity.java
@@ -0,0 +1,13 @@
+package navigation_drawer.noeurng.com.intro_slide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
